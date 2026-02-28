1248609b8cdf4639a3e6875cef3186740ae5622f
Jiří Král <jirkakral978@protonmail.com>
2026-02-28 15:24:37 +0100

    modified hud and building

--- scripts/building.gd
+++ scripts/building.gd
@@ -4,7 +4,7 @@ extends Panel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	building.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
