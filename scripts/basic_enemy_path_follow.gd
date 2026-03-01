1df25c897876d8a1390d3fc392ffbff8a2157c46
Patrik Pánek <panekpat@cvut.cz>
2026-03-01 09:31:39 +0100

    enemy fall hole haha

--- scripts/basic_enemy_path_follow.gd
+++ scripts/basic_enemy_path_follow.gd
@@ -11,6 +11,24 @@ func _process(delta):
 	if progress_ratio >= 1.0:
 		reached_end()
 
+func _on_trap_detector_area_entered(area: Area2D):
+	# We know it's a hole because our Mask is set to Layer 4 (Traps)
+	fall_into_hole()
+
+func fall_into_hole():
+	# 1. Stop the enemy from moving
+	set_process(false)
+	
+	# 2. Visual "Falling" effect
+	var tween = create_tween()
+	# Shrink and rotate while disappearing
+	tween.set_parallel(true)
+	tween.tween_property(self, "scale", Vector2.ZERO, 0.3)
+	tween.tween_property(self, "modulate:a", 0.0, 0.3)
+	
+	# 3. Remove the enemy when the animation is done
+	tween.set_parallel(false)
+	tween.finished.connect(queue_free)
 func reached_end():
 	print("U ded")
 	queue_free() # Remove the enemy
