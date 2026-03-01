3eff4c980300100f48224c99f697bae5a75f0fa5
Patrik Pánek <panekpat@cvut.cz>
2026-03-01 08:12:45 +0100

    minor changes

--- scripts/building_manager.gd
+++ scripts/building_manager.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var tile_map: TileMap = null
-var selected_building_path # Path to the PackedScene (.tscn)
+var selected_building_path = "" # Path to the PackedScene (.tscn)
 var ghost_building: Sprite2D = null
 var current_building_resource: PackedScene = null
 
@@ -51,27 +51,37 @@ func _process(_delta):
 	ghost_building.show()
 	
 	# Check if the tile is valid (e.g., is there a tile there at all?)
-	var tile_data = tile_map.get_cell_tile_data(0, cell_pos)
-	if tile_data == null:
-		ghost_building.modulate = Color(10, 0, 0, 0.5) # Red tint if invalid
+	var data = tile_map.get_cell_tile_data(0, cell_pos)
+	var is_valid = true
+
+	# Check path requirement for the preview
+	if data:
+		var is_path_tile = data.get_custom_data("is_path")
+		# If the building path is a 'hole', and it's not a path tile...
+		if selected_building_path.contains("hole") and not is_path_tile:
+			is_valid = false
+
+	if is_valid:
+		ghost_building.modulate = Color(1, 1, 1, 0.5) # Blue/White tint
 	else:
-		ghost_building.modulate = Color(1, 1, 1, 0.5) # White/Normal tint
+		ghost_building.modulate = Color(1, 0, 0, 0.5) # Red tint (Invalid!)
 		
 func _input(event: InputEvent):
 	if not can_build:
 		return
 	# 1. Check if we have something selected and the user clicked
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		print("Tile cliked.")
+		print("Tile clicked.")
 		if selected_building_path != "":
 			place_building()
 
 func place_building():
 	var mouse_pos = get_global_mouse_position()
 	var cell_pos = tile_map.local_to_map(tile_map.to_local(mouse_pos))
-	
+	var data = tile_map.get_cell_tile_data(0, cell_pos)
+	var can_place_here:bool = true
 	# VERIFICATION: Only place if a valid tile exists
-	if tile_map.get_cell_tile_data(0, cell_pos) == null:
+	if data == null:
 		print("Cannot build here: No tile found!")
 		return
 
@@ -79,6 +89,18 @@ func place_building():
 	var center_pos = tile_map.map_to_local(cell_pos)
 	var building = current_building_resource.instantiate()
 	building.z_index = 10
-	building.global_position = tile_map.to_global(center_pos)
-	add_child(building)
+	if building.needs_path == true:
+			var is_path_tile = data.get_custom_data("is_path")
+			print(is_path_tile)
+			if is_path_tile != true:
+				can_place_here = false
+				print("PLACEMENT BLOCKED: Not a path tile.")
+			else:
+				can_place_here = true
+
+	if can_place_here:
+		center_pos = tile_map.map_to_local(cell_pos)
+		building.global_position = tile_map.to_global(center_pos)
+		add_child(building)
+
 	selected_building_path = ""
--- scripts/hole.gd
+++
@@ -1,4 +0,0 @@
-extends Button
-
-func _on_pressed() -> void:
-	GameEvents.building_selected.emit("res://scenes/buildings/hole.tscn")
