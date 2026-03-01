eb9858661bdaf3762c8a74332dbca59b54f86be2
Patrik Pánek <panekpat@cvut.cz>
2026-03-01 16:21:58 +0100

    audio settings

--- scripts/settings_menu_canvas.gd
+++ scripts/settings_menu_canvas.gd
@@ -3,8 +3,10 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	var music_db = AudioServer.get_bus_volume_db(music_bus_id)
+	var sfx_db = AudioServer.get_bus_index("SFX")
+	$VBoxContainer/Music.value = db_to_linear(music_db)
+	$VBoxContainer/SFX.value = db_to_linear(AudioServer.get_bus_volume_db(sfx_bus_id))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -17,3 +19,16 @@ func _on_back_to_menu_button_pressed() -> void:
 
 func _on_easter_egg_button_pressed() -> void:
 	$SettingsMenuController/RichTextLabel.visible = true
+
+@onready var music_bus_id = AudioServer.get_bus_index("Music")
+@onready var sfx_bus_id = AudioServer.get_bus_index("SFX")
+func _on_music_slider_value_changed(value: float) -> void:
+	# linear_to_db converts 0.0-1.0 to -80dB to 0dB
+	AudioServer.set_bus_volume_db(music_bus_id, linear_to_db(value))
+	
+	# Mute the bus if the slider is at 0
+	AudioServer.set_bus_mute(music_bus_id, value < 0.01)
+
+func _on_sfx_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(sfx_bus_id, linear_to_db(value))
+	AudioServer.set_bus_mute(sfx_bus_id, value < 0.01)
